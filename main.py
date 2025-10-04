3a2ffe4e9f6eef5fb6b78af5f49da6f858d4af5c
pablo-delaunay <pablo.delaunay@edu.esiee.fr>
2025-10-04 09:16:52 +0000

    .

--- main.py
+++ main.py
@@ -14,38 +14,52 @@ def read_data(filename):
         list: le contenu du fichier (1 list par ligne)
     """
     l = []
+
+    with open(filename, mode="r", encoding="utf8") as file:
+        lines = file.readlines()
+    for line in lines:
+        line = line.strip() 
+        str_values = line.split(';')  
+        int_values = [int(x) for x in str_values] 
+        l.append(int_values)
+
     return l
 
-def get_list_k(data, k):
-    l = []
-    return l
+def get_list_k(l, k):
+    return l[k]
 
 def get_first(l):
-    return None
+    return l[0]
 
 def get_last(l):
-    return None
+    return l[-1]
 
 def get_max(l):
-    return None
+    return max(l)
 
 def get_min(l):
-    return None
+    return min(l)
 
 def get_sum(l):
-    return None
+    return sum(l)
 
 
 #### Fonction principale
 
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
+    k = 37
+    l_k = get_list_k(data, k)
+    print(f"\nListe {k} :", l_k)
+    print("Premier :", get_first(l_k))
+    print("Dernier :", get_last(l_k))
+    print("Max :", get_max(l_k))
+    print("Min :", get_min(l_k))
+    print("Somme :", get_sum(l_k))
+    
 
 
 if __name__ == "__main__":
